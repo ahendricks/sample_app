2190f8e0fbe54430c00941d72b30a379fae4f244
Alex Hendricks <alex@hendricksmail.com>
2014-03-03 20:51:31 -0800

    Change before_save callback, add downcase test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
 		it { should_not be_valid }
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "FoO@bAr.com" }
+
+		it "should be save with all lower case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
 	describe "when password is not present" do
 		before do
 			@user.password = @user.password_confirmation = " "
